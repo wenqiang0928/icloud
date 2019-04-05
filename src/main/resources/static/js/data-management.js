0ed13b99270fb7451717cf76fe41dc62913ec36f
SpongeBob90 <1442772080@qq.com>
2019-04-05 14:25:57 +0800

    功能列表加图标、上传弹框

--- src/main/resources/static/js/data-management.js
+++ src/main/resources/static/js/data-management.js
@@ -24,3 +24,15 @@ function signUp() {
     };
     $("body").load(url, params);
 }
+
+function uploadModal() {
+    
+}
+
+function centerModals() {
+    var $clone = $(this).clone().css('display','block').appendTo('body');
+    var top = Math.round(($clone.height() - $clone.find('.modal-content').height()) / 2);
+    top = top > 0 ? top : 0;
+    $clone.remove();
+    $(this).find('.modal-content').css("margin-top", top);
+}
